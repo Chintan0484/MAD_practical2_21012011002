c7694b7b1cd2797577b00031faa6edb09862a142
Chintan Aparnathi <chintanaparnathi48@gmail.com>
2023-07-30 23:51:43 +0530

    ca

--- app/src/main/java/com/example/mad_practical2_21012011002/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical2_21012011002/MainActivity.kt
@@ -5,13 +5,21 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity(){
     val TAG = "MainActivity"
     fun showMassage(message:String){
         Log.i(TAG, message)
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        val constraint: ConstraintLayout? = findViewById(R.id.mainConstraint)
+        if (constraint!=null)
+        {
+            Snackbar.make(constraint,message,Snackbar.LENGTH_SHORT).show()
+        }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,5 +40,25 @@ class MainActivity : AppCompatActivity(){
         super.onResume()
         showMassage("onresume method is called")
         
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        showMassage("onRestart method is called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        showMassage("onPause method is called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        showMassage("onStop method is called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        showMassage("onDestroy method is called")
     }
 }
